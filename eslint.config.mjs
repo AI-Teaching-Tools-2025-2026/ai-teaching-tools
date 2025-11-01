9af6103444f2354b115f9a26bf8f22402c74b725
Katie Kimura <katiekimura2022@gmail.com>
2025-11-01 14:29:47 -0700

    Finished the Code Style, PRs and Reviews, CI/CD, Security, and Documentation in Contributing.md; Set up ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules, // disables conflicting ESLint rules
+      "prettier/prettier": ["error"], // runs Prettier as an ESLint rule
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
