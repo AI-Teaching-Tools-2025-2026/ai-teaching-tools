ccb10b487524258ca22dc15950e537d5354f186f
dliu04 <dliuninja@gmail.com>
2025-11-06 13:56:52 -0800

    wip

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,40 @@
+import { Container, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Container>
+      <Typography 
+        variant="h1"
+        sx={{ fontSize: '2rem', fontWeight: 'bold', marginTop: '2rem', textAlign: 'center' }}
+      >
+        Login Page
+      </Typography>
+
+      <Box
+        component="form"
+        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            disabled
+            id="outlined-disabled"
+            label="Disabled"
+            defaultValue="Username"
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+        </div>
+      </Box>
+
+    </Container>
   );
 }
