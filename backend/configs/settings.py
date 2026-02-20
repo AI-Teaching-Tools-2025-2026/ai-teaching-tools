db358c588925a144711122c81c70d7bfaaa25820
dliu04 <dliuninja@gmail.com>
2026-02-19 18:52:22 -0800

    Tweak config file to use .env file from root directory.

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -1,8 +1,15 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
+
+# This points to backend/configs
+CURRENT_FILE_DIR = Path(__file__).resolve().parent
+# This points to the root of the project (two levels up from configs -> backend -> root)
+ROOT_DIR = CURRENT_FILE_DIR.parent.parent
 
 class AppSettings(BaseSettings):
     class Config:
-        env_file = ".env"
+        # Point to the .env file in the root directory
+        env_file = str(ROOT_DIR / ".env")
         env_file_encoding = "utf-8"
         env_prefix = "app_"
 
