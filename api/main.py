792f4e7cc1df650bd1ba4d8cd1731096a367592a
Annabel Wang <annabelwyn11@gmail.com>
2026-04-15 13:48:16 -0700

    fix

--- api/main.py
+++ api/main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# This tells Vercel to treat the 'api' folder as the root for imports
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
